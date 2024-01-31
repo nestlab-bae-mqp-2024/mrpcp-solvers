2d9a844c8a0326e370a6a26f02c89b8dd321a7b5
Samara Holmes <46973232+holmes1000@users.noreply.github.com>
2024-01-31 05:21:41 -0500

    3 server threads, receiving k on 12001

--- serverComms/client.py
+++ serverComms/client.py
@@ -1,9 +1,27 @@
 from socket import *
-serverName = '127.0.0.1'
-serverPort = 12001
-clientSocket = socket(AF_INET, SOCK_DGRAM)
+
+def choose_server_port(server_choice):
+    if server_choice == "1":
+        return 12001
+    elif server_choice == "2":
+        return 12002
+    elif server_choice == "3":
+        return 12003
+    else:
+        print("Invalid server choice.")
+        exit(1)
+
+server_choice = input("Choose server ('(1) solve', '(2) available_jobs', or '(3) get_solution'): ")
+serverPort = choose_server_port(server_choice)
+
+clientSocket = socket(AF_INET, SOCK_STREAM)
+
+clientSocket.connect(('127.0.0.1', serverPort))
+
 message = input('Input number of robots: ')
-clientSocket.sendto(message.encode(), (serverName, serverPort))
-modMessage, serverAddress = clientSocket.recvfrom(2048)
-print('From server: ', modMessage.decode())
+clientSocket.send(message.encode())
+
+modMessage = clientSocket.recv(2048).decode()
+print('From server:', modMessage)
+
 clientSocket.close()
