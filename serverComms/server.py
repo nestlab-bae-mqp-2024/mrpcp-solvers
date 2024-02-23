a7a19053730258c79d6af6935f6bae13049475cf
Samara Holmes <46973232+holmes1000@users.noreply.github.com>
2024-02-23 11:06:44 -0500

    code cleanup, beginning implementation of cams recalc

--- serverComms/server.py
+++ serverComms/server.py
@@ -21,12 +21,11 @@ from serverComms.recalculate import recalculate_paths
 
 app = Flask(__name__)
 
-
-"""
-This function defines the '/solve' endpoint for solving MILP or Heuristic problems.
-"""
 @app.route('/solve', methods=['POST'])
 def solve_endpoint():
+    """
+    This function defines the '/solve' endpoint for solving MILP or Heuristic problems.
+    """
     print("Solution request received.")
     k = request.args.get('k')
     q_k = request.args.get('q_k')
@@ -56,67 +55,79 @@ def solve_endpoint():
         print(f"Contents of the JSON file: {result}")  # Print contents of JSON file
         return jsonify(result), 200
 
-
-    print(f"Job folder does not exist: {job_folder_path}. Starting solver function with parameters k={k}, q_k={q_k}, n={n_a}, rp={rp}, l={l}, d={d}, mode={mode}...")
+    print(
+        f"Job folder does not exist: {job_folder_path}. Starting solver function with parameters k={k}, q_k={q_k}, n={n_a}, rp={rp}, l={l}, d={d}, mode={mode}...")
     # Run MILP solver function with parameters on a separate thread
     solve_thread = threading.Thread(target=run_solver, args=(k, q_k, n_a, rp, l, d, mode, job_id))
     solve_thread.start()
-    result_data = {'job_id': job_id, 'params': {'k': k, 'q_k': q_k, 'n_a': n_a, 'rp': rp, 'l': l, 'd': d, 'mode': 'h'}, 'robot_node_path': 'in progress', 'robot_world_path': 'in progress', 'status': 'in progress'}
+    result_data = {'job_id': job_id, 'params': {'k': k, 'q_k': q_k, 'n_a': n_a, 'rp': rp, 'l': l, 'd': d, 'mode': 'h'},
+                   'robot_node_path': 'in progress', 'robot_world_path': 'in progress', 'status': 'in progress'}
     return jsonify(result_data), 200
 
 
-"""
-This function runs the MILP solver function with the provided parameters.
-Once the solver completes, it saves the result to a JSON file in the cache folder.
-Params:
-    k (int): Number of paths
-    q_k (float): Maximum number of paths
-    n_a (int): Number of nodes per axis
-    rp (int): redundancy parameter
-    l (float): 
-    d (float): distance between nodes
-    job_id (str): Job ID
-"""
 def run_solver(k, q_k, n_a, rp, l, d, mode, job_id):
+    """
+    This function runs the MILP solver function with the provided parameters.
+    Once the solver completes, it saves the result to a JSON file in the cache folder.
+    Params:
+        k (int): Number of paths
+        q_k (float): Maximum number of paths
+        n_a (int): Number of nodes per axis
+        rp (int): redundancy parameter
+        l (float):
+        d (float): distance between nodes
+        job_id (str): Job ID
+    """
     try:
         if mode == 'm':
             # Run MILP solver function with parameters
-            print(f"Running MILP solver function with parameters: k={k}, q_k={q_k}, n={n_a}, rp={rp}, l={l}, d={d}, mode=m...")
-            edges, world = solve_milp_with_optimizations(int(k), float(q_k), int(n_a), int(rp), float(l), float(d), job_id)
-            print(f"MILP solver function completed with parameters: k={k}, q_k={q_k}, n={n_a}, rp={rp}, l={l}, d={d}, mode=m.")
+            print(
+                f"Running MILP solver function with parameters: k={k}, q_k={q_k}, n={n_a}, rp={rp}, l={l}, d={d}, mode=m...")
+            edges, world = solve_milp_with_optimizations(int(k), float(q_k), int(n_a), int(rp), float(l), float(d),
+                                                         job_id)
+            print(
+                f"MILP solver function completed with parameters: k={k}, q_k={q_k}, n={n_a}, rp={rp}, l={l}, d={d}, mode=m.")
             # Convert edges to a node format
             robot_node_path = [[int(edge) for edge in path] for path in edges]
             robot_world_path = world
 
             # Save result in a JSON file within the cache folder
-            result_data = {'job_id': job_id, 'params': {'k': k, 'q_k': q_k, 'n_a': n_a, 'rp': rp, 'l': l, 'd': d, 'mode': 'h'}, 'robot_node_path': robot_node_path, 'robot_world_path': robot_world_path, 'status': 'completed'}
-            json_handlers.saveResultsToCache(job_id, result_data, 'result.json') # Save the results to the cache
+            result_data = {'job_id': job_id,
+                           'params': {'k': k, 'q_k': q_k, 'n_a': n_a, 'rp': rp, 'l': l, 'd': d, 'mode': 'h'},
+                           'robot_node_path': robot_node_path, 'robot_world_path': robot_world_path,
+                           'status': 'completed'}
+            json_handlers.saveResultsToCache(job_id, result_data, 'result.json')  # Save the results to the cache
 
         elif mode == 'h':
             # Run Heuristic solver function with parameters
-            print(f"Running Heuristic solver function with parameters: k={k}, q_k={q_k}, n={n_a}, rp={rp}, l={l}, d={d}, mode=h...")
+            print(
+                f"Running Heuristic solver function with parameters: k={k}, q_k={q_k}, n={n_a}, rp={rp}, l={l}, d={d}, mode=h...")
             edges, world = solve_mrpcp_heuristic(int(k), float(q_k), int(n_a), int(rp), float(l), float(d), job_id)
-            print(f"Heuristic solver function completed with parameters: k={k}, q_k={q_k}, n={n_a}, rp={rp}, l={l}, d={d}, mode=h.")
+            print(
+                f"Heuristic solver function completed with parameters: k={k}, q_k={q_k}, n={n_a}, rp={rp}, l={l}, d={d}, mode=h.")
 
             # Convert edges to a node format
             robot_node_path = [[int(edge) for edge in path] for path in edges]
             robot_world_path = world
 
             # Save result in a JSON file within the cache folder
-            result_data = {'job_id': job_id, 'params': {'k': k, 'q_k': q_k, 'n_a': n_a, 'rp': rp, 'l': l, 'd': d, 'mode': 'h'}, 'robot_node_path': robot_node_path, 'robot_world_path': robot_world_path, 'status': 'completed'}
-            json_handlers.saveResultsToCache(job_id, result_data, 'result.json') # Save the results to the cache
+            result_data = {'job_id': job_id,
+                           'params': {'k': k, 'q_k': q_k, 'n_a': n_a, 'rp': rp, 'l': l, 'd': d, 'mode': 'h'},
+                           'robot_node_path': robot_node_path, 'robot_world_path': robot_world_path,
+                           'status': 'completed'}
+            json_handlers.saveResultsToCache(job_id, result_data, 'result.json')  # Save the results to the cache
 
-            return result_data # Return the content of the JSON file
+            return result_data  # Return the content of the JSON file
 
     except Exception as e:
         print(f"Error occurred during solving: {e}")
 
 
-"""
-This function defines the '/recalculate' endpoint for recalculating the paths
-"""
 @app.route('/recalculate', methods=['POST'])
 def recalc_endpoint():
+    """
+    This function defines the '/recalculate' endpoint for recalculating the paths
+    """
     print("Recalculation request received.")
     job_id = request.args.get('job_id')
     curr_robots_pos = request.args.get('curr_robots_pos')
@@ -124,7 +135,7 @@ def recalc_endpoint():
     failed_robot_id = request.args.get('failed_robot_id')
 
     # Run the function to recalculate the paths based on the input parameters
-    return recalculate_paths(job_id, curr_robots_pos, failed_robot_id) # Return the json result of the recalculation
+    return recalculate_paths(job_id, curr_robots_pos, failed_robot_id)  # Return the json result of the recalculation
 
 
 if __name__ == '__main__':
