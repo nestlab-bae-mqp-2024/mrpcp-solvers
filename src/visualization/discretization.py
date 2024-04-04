ca5a4b0cf25af77d4915572968f47d0da20d44b5
Camden Cummings <camden.e.cummings@gmail.com>
2024-04-04 13:53:11 -0400

    also

---
+++ src/visualization/discretization.py
@@ -0,0 +1,23 @@
+import numpy as np
+import itertools
+
+def discretize_world_points(all_world_points, metadata):
+    ssd = metadata["ssd"]
+    nk = metadata["nk"]
+    k = metadata["k"]
+    dist_betw_each_node = ssd/(nk*k-1)
+    one_side = np.arange(-ssd/2, ssd/2 + ssd/(nk*k-1), dist_betw_each_node)
+
+    all_nodes = [(x,y) for x in one_side for y in one_side]
+
+    discretized_points = [[] for ki in range(0,k)]
+    for ki in range(0,k):
+        for position in all_world_points[ki]:
+            last_posn = (0,0)
+            for node in all_nodes:
+                if(abs(last_posn[0]-position[0]) > 0.05 and abs(last_posn[1]-position[1]) > 0.05 and (abs(position[0] - node[0]) < 0.05 and abs(position[1] - node[1]) < 0.05)):
+                    last_posn = position
+                    discretized_points[ki].append((node[0], node[1]))
+
+    return discretized_points
+    #print(all_nodes)
